88280a79ba11260a160b7d129d518b2a74232aaf
Nour ASLAOUI <43506141+Nour963@users.noreply.github.com>
2020-09-07 13:24:36 +0100

    Update resolvconf.sh

--- resolvconf.sh
+++ resolvconf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Make /etc/resolv.conf permanent
 rm /etc/resolv.conf
-apt install resolvconf -y
+apt-get install resolvconf -y
 echo"
 nameserver 192.168.2.82
 search ims.mnc001.mcc001.3gppnetwork.org" > /etc/resolvconf/resolv.conf.d/base
