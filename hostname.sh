3d9dd11b752bc89fb43f31c40883b428205a4009
Nour ASLAOUI <43506141+Nour963@users.noreply.github.com>
2020-09-14 15:05:28 +0100

    Update hostname.sh

--- hostname.sh
+++ hostname.sh
@@ -7,3 +7,19 @@ sed -i 's/ubuntu/testy/g' /etc/hosts
 #sed -i '2i 127.0.1.1 testy' /etc/hosts
 hostnamectl set-hostname testy
 exec bash
+
+###########################
+#increment the last number of the name exp student1 becomes student2
+############################
+
+OLDNAME=$(hostname)
+OLDNUM=$(hostname | grep -Eo '[[:digit:]]+')
+NEWNUM=$((OLDNUM+1))
+NEWNAME=$(echo ${OLDNAME/$OLDNUM/$NEWNUM})
+sed -i "s/$OLDNAME/$NEWNAME/g" /etc/hostname
+#in case where 127.0.1.1 is already set
+sed -i "s/$OLDNAME/$OLDNAME/g" /etc/hosts
+#in case where 127.0.1.1 is not set
+#sed -i "2i 127.0.1.1 $NEWNAME" /etc/hosts
+hostnamectl set-hostname $NEWNAME
+exec bash
