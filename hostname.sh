d2649d11d3bbe329b6f5d74202756977dc820aab
Nour ASLAOUI <43506141+Nour963@users.noreply.github.com>
2020-09-14 13:45:59 +0100

    Update hostname.sh

--- hostname.sh
+++ hostname.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #change hostname machine without restart
 echo "testy" > /etc/hostname
-sed -i '1 127.0.1.1 testy' /etc/hosts
+sed -i '1i 127.0.1.1 testy' /etc/hosts
 hostnamectl set-hostname testy
 exec bash
