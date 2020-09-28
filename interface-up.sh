62f89b552b5d6bd9ec55b352726414f360343a0a
Nour ASLAOUI <43506141+Nour963@users.noreply.github.com>
2020-09-28 18:47:45 +0100

    Update interface-up.sh

--- interface-up.sh
+++ interface-up.sh
@@ -4,13 +4,11 @@
 VERSION=$(lsb_release -r -s)
 if [[ $VERSION =~ ["^18"] ]]
 then
-  echo 'auto ens4\niface ens4 inet dhcp' | sudo tee /etc/network/interfaces > /dev/null
+  echo 'auto ens4\niface ens4 inet dhcp' | tee /etc/network/interfaces > /dev/null
   apt install ifupdown -qqy
   ifup ens4
 elif [[ $VERSION =~ ["^16"] ]]
 then
-  echo "
-  auto ens4
-  iface ens4 inet dhcp" >> /etc/network/interfaces.d/50-cloud-init.cfg
+  echo 'auto ens4\niface ens4 inet dhcp' | tee /etc/network/interfaces.d/50-cloud-init.cfg > /dev/null
   /etc/init.d/networking restart
 fi
