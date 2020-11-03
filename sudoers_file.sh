fb9925943c2051008e6f2dea9b5ef54c1fa1e0f7
Nour ASLAOUI <43506141+Nour963@users.noreply.github.com>
2020-11-03 11:20:58 +0100

    Update sudoers_file.sh

--- sudoers_file.sh
+++ sudoers_file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NF_LIST="nrf amf pcrf udr udm ausf upf sgwu smf sgwc mme hss"
-VAR=(.service " ")
+VAR=(.service " ") #example: 'systemctl start sshd.service' AND 'systemctl start sshd'
 AC="start restart stop status"
 echo -n "ubuntu ALL=(ALL) NOPASSWD: /sbin/ip, /bin/kill, /usr/bin/pkill, /usr/sbin/tcpdump, /bin/netstat, " > /etc/sudoers.d/90-cloud-init-users
 for NF in ${NF_LIST}; do
