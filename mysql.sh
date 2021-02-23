5ff705d6472c0911fbbaec8b3c0825c7746da96f
Nour ASLAOUI <43506141+Nour963@users.noreply.github.com>
2021-02-23 15:39:46 +0100

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#configure mysql and provision it
+#configure mysql on centos and provision it
 export MYPWD="Hello123?";
 sudo mysql_secure_installation 2>/dev/null <<MSI
 n
@@ -20,3 +20,49 @@ CREATE USER 'nour'@'%' IDENTIFIED BY 'Hello123?';
 GRANT ALL PRIVILEGES ON *.* TO 'nour'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 MSI2
+#########################################################################################
+
+##configure mysql on UBUNTU16 and provision it
+MYSQL_ROOT_PASSWORD='D33Ps3CR3T?'
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+echo debconf mysql-server/root_password password $MYSQL_ROOT_PASSWORD | \
+  debconf-set-selections
+echo debconf mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD | \
+  debconf-set-selections
+apt-get -yqq install mysql-server > /dev/null 
+apt-get -yqq install expect > /dev/null
+tee ~/secure_our_mysql.sh > /dev/null << EOF
+spawn $(which mysql_secure_installation)
+
+expect "Enter password for user root:"
+send "$MYSQL_ROOT_PASSWORD\r"
+
+expect "Press y|Y for Yes, any other key for No:"
+send "N\r"
+
+expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
+send "N\r"
+
+expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+EOF
+expect ~/secure_our_mysql.sh
+rm -v ~/secure_our_mysql.sh
+mysql -u root -p --password=D33Ps3CR3T? --execute="update user set authentication_string='' where User='root';flush privileges;" mysql
+/etc/init.d/mysql stop
+/etc/init.d/mysql start
+mysql -u root --execute="uninstall plugin validate_password;"
+
